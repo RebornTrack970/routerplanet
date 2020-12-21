32470a0be01d6af7756e4a0010ba029c6fb49906
QmelZ <qmelzthevoid@gmail.com>
2020-12-21 14:31:48 +0300

    un-h

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ Events.on(ClientLoadEvent, () => {
     routros.alwaysUnlocked = true;
     routros.localizedName = "Router";
 
-    for(var i = 0, i < 31, i++){
+    for(var i = 0; i < 31; i++){
         var h = new SectorPreset("router", routros, i);
         h.localizedName = "Router" + i;
         h.alwaysUnlocked = true;
